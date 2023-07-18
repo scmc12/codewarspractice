e46f55e5388e948fb46f390c844a59b8d098fb25
mcgh12 <mcguire.holt@yahoo.com>
2023-07-18 17:14:37 -0600

    New repo

---
+++ main.js
@@ -0,0 +1,55 @@
+
+switch.(welcomeToPage){
+case english:
+message = 'Welcome';
+break;
+case czech:
+message = 'Vitejte';
+break;
+case danish: 
+message = 
+message = 'Velkomst';
+break;
+case dutch: 
+message = 'Welkom';
+break;
+case estonian: 
+message = 'Tere tulemast';
+break;
+case finnish: 
+message = 'Tervetuloa';
+break;
+case flemish: 
+message = 'Welgekomen';
+break;
+case french: 
+message = 'Bienvenue';
+break;
+case german: 
+message = 'Willkommen';
+break;
+case irish: 
+message = 'Failte';
+break;
+case italian: 
+break;
+message = 'Benvenuto';
+break;
+case latvian: 
+message = 'Gaidits';
+break;
+case lithuanian: 
+message = 'Laukiamas';
+break;
+case polish: 
+message = 'Witamy';
+break;
+case spanish: 
+message = 'Bienvenido';
+break;
+case swedish: 
+message = 'Valkommen';
+break;
+case welsh: 
+message = 'Croeso'
+}

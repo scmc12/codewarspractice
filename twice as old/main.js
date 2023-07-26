d5a88b3d11dbde4c70b4126d2a6a348dc8af5423
mcgh12 <mcguire.holt@yahoo.com>
2023-07-26 12:20:16 -0500

    refactor

--- twice as old/main.js
+++ twice as old/main.js
@@ -1,5 +1,3 @@
 twiceAsOld = (dadYearsOld, sonYearsOld) => {
-  const ageAtBirth = dadYearsOld - sonYearsOld;
-  const doubleAge = ageAtBirth * 2;
-  return Math.abs(dadYearsOld - doubleAge);
+  return Math.abs(dadYearsOld - 2 * sonYearsOld);
 };
